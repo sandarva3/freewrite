c8d123f5a39706265280d793c5c59628471d59fa
sandarva3 <sandarvapoudel182@gmail.com>
2025-05-19 21:29:11 +0545

    more bug fixes

--- linux/main.py
+++ linux/main.py
@@ -1,14 +1,19 @@
 import sys
+import os
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import QLocale# Add QLocale import
-from freewriteApp import FreewriteApp
+from PyQt5.QtCore import QLocale
 
+# Add BASE_DIR for absolute paths
+BASE_DIR = "/usr/share/freewrite"
+
+from freewriteApp import FreewriteApp
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     # Force LTR locale (English, US)
     locale = QLocale(QLocale.English, QLocale.UnitedStates)
     QLocale.setDefault(locale)
-    with open("assets/style.qss", "r") as f:
+    # Use absolute path here
+    with open(os.path.join(BASE_DIR, "assets/style.qss"), "r") as f:
         app.setStyleSheet(f.read())
     window = FreewriteApp()
     window.show()
